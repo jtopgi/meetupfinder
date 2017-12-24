a0347112af88ea091c1882e81707f0394c4a0260
jtopgi <jtopgi@users.noreply.github.com>
2017-12-24 01:41:35 -0600

    Made working frontend and everything works.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /* boilerplate code */
 var express = require('express');
 var request = require('request');
+var moment = require('moment');
 
 var meetupKey = '407514572207356a7a291a1d324f9'
 var googleKey = 'AIzaSyDJmNlBAaUW46Wi36wS1MONYb2I9hsTKs4';
@@ -15,24 +16,30 @@ app.set('port', 3000);
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-  request('https://api.meetup.com/2/open_events?&sign=true&photo-host=public&zip=60657&radius=5&page=20&key=' + meetupKey, async function(error, response, body) {
+  var interval = moment.utc().startOf('day') + ',' + moment.utc().endOf('day');
+  console.log(interval);
+  request('https://api.meetup.com/2/open_events?&sign=true&photo-host=public&zip=60657&time='+ interval +'&radius=5&&key=' + meetupKey, async function(error, response, body) {
     var data = JSON.parse(body)
     var events = [];
-    var location, time, url;
+    var location, travelTime, url;
 
-    
-
-
-    for (var i = 11; i < data.meta.count; i++) {
+    for (var i = 0; i < data.meta.count; i++) {
+      if (data.results[i].venue === undefined || data.results[i].waitlist_count > 0) {
+        continue;
+      }
       console.log(data.results[i]);
-      location = data.results[i]['venue']['lat'] + ',' + data.results[i]['venue']['lon'];
-      time = await getTime(location);
+      location = data.results[i].venue.lat + ',' + data.results[i].venue.lon;
+      travelTime = await getTime(location);
       events.push({
-        name: data.results[i]['name'],
+        groupName: data.results[i].group.name,
+        eventName: data.results[i].name,
         location: location,
-        time: time
+        travelTime: travelTime,
+        url: data.results[i].event_url,
+        startTime: moment(data.results[i].time).format('h:mm a')
       });
     }
+
     var context = {};
     context.events = events;
     res.render('home', context);
@@ -42,10 +49,13 @@ app.get('/', function(req, res) {
 function getTime(location) {
   return new Promise(function(resolve, reject) {
     request('https://maps.googleapis.com/maps/api/directions/json?origin=' + location + '&destination=41.939917,-87.652890&mode=transit&key=' + googleKey, function(error, response, body) {
-      resolve(5);
+      if (JSON.parse(body).status == 'OK') {
+        resolve(JSON.parse(body).routes[0].legs[0].duration.text.replace(/\D/g,''));
+      }
     });
   });
 }
+
 
 app.listen(app.get('port'), function() {
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
