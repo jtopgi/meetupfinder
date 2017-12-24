eba5db52037466b6de076f42bf46c47a248314ff
jtopgi <jtopgi@users.noreply.github.com>
2017-12-24 13:58:27 -0600

    Cleaned up api keys and unnecessary node modules.

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-/* boilerplate code */
 var express = require('express');
 var request = require('request');
 var moment = require('moment');
 
-var meetupKey = '407514572207356a7a291a1d324f9'
-var googleKey = 'AIzaSyDJmNlBAaUW46Wi36wS1MONYb2I9hsTKs4';
 var app = express();
 var handlebars = require('express-handlebars').create({
   defaultLayout: 'main'
 });
+require('dotenv').load();
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -16,9 +14,8 @@ app.set('port', 3000);
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-  var interval = moment.utc().startOf('day') + ',' + moment.utc().endOf('day');
-  console.log(interval);
-  request('https://api.meetup.com/2/open_events?&sign=true&photo-host=public&zip=60657&time='+ interval +'&radius=5&&key=' + meetupKey, async function(error, response, body) {
+  var interval = moment.utc() + ',' + moment.utc().endOf('day');
+  request('https://api.meetup.com/2/open_events?&sign=true&photo-host=public&zip=60657&time='+ interval +'&radius=5&&key=' + process.env.meetupKey, async function(error, response, body) {
     var data = JSON.parse(body)
     var events = [];
     var location, travelTime, url;
@@ -27,7 +24,6 @@ app.get('/', function(req, res) {
       if (data.results[i].venue === undefined || data.results[i].waitlist_count > 0) {
         continue;
       }
-      console.log(data.results[i]);
       location = data.results[i].venue.lat + ',' + data.results[i].venue.lon;
       travelTime = await getTime(location);
       events.push({
@@ -48,7 +44,7 @@ app.get('/', function(req, res) {
 
 function getTime(location) {
   return new Promise(function(resolve, reject) {
-    request('https://maps.googleapis.com/maps/api/directions/json?origin=' + location + '&destination=41.939917,-87.652890&mode=transit&key=' + googleKey, function(error, response, body) {
+    request('https://maps.googleapis.com/maps/api/directions/json?origin=' + location + '&destination=' + process.env.destination + '&mode=transit&key=' + process.env.googleKey, function(error, response, body) {
       if (JSON.parse(body).status == 'OK') {
         resolve(JSON.parse(body).routes[0].legs[0].duration.text.replace(/\D/g,''));
       }
