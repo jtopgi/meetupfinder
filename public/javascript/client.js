5a2a4421b82077ccda58424cc8d92a2bc864c71c
jtopgi <jtopgi@users.noreply.github.com>
2017-12-29 00:26:14 -0600

    Added nicer date picker.

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -1,3 +1,9 @@
+$('.input-daterange').datepicker({
+    format: "yyyy-mm-dd",
+    autoclose: true,
+    todayHighlight: true
+});
+
 $('.date').each(function(){
   $(this).text(moment(parseInt($(this).text())).local().format('MM/DD'));
 });
